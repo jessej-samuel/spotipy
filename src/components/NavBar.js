1074ad43d78efc0d4f9b76f82b4e4595c5836c6f
Jessej Samuel <1234jessej@gmail.com>
2022-01-07 14:46:12 +0530

    Fix: Link open in new tab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,7 +8,11 @@ const NavBar = () => {
             <i className="fab fa-spotify"></i>
             <div className="app-header">{metadata.appName}</div>
             <div className="nav-links">
-                <a href="https://www.github.com/jessej-samuel/spotipy">
+                <a
+                    href="https://www.github.com/jessej-samuel/spotipy"
+                    target="_blank"
+                    rel="noreferrer"
+                >
                     <i className="fab fa-github"></i>
                 </a>
             </div>
